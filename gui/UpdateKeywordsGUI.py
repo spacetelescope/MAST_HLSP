5507ca49c75a1dacc236b6b605298e91a8789461
Peter Forshay <pforshay@stsci.edu>
2018-08-28 17:40:30 -0400

    Mainly working on creating GUI for updating FITS keyword properties.

--- gui/UpdateKeywordsGUI.py
+++ gui/UpdateKeywordsGUI.py
@@ -1,12 +1,12 @@
-import yaml
-
 import gui.GUIbuttons as gb
 import os
 import sys
 
 from bin.read_yaml import read_yaml
+from CHECK_METADATA_FORMAT.check_metadata_format import check_metadata_format
+from CHECK_METADATA_FORMAT.precheck_data_format import precheck_data_format
+from lib.FileType import FileType
 from lib.FitsKeyword import FitsKeyword
-from lib.HLSPFile import HLSPFile
 
 try:
     from PyQt5.QtCore import *
@@ -15,66 +15,179 @@ except ImportError:
     from PyQt4.QtCore import *
     from PyQt4.QtGui import *
 
+TEMPLATES_DIR = "CHECK_METADATA_FORMAT/TEMPLATES"
+
 
 class UpdateKeywordsGUI(QWidget):
 
     def __init__(self, parent):
-        self.master = parent
-        self.product_type = []
-        self.standard = []
-        for ftype in self.master.hlsp.file_types:
-            if ftype["ProductType"] not in self.product_type:
-                self.product_type.append(ftype["ProductType"])
-            if ftype["Standard"] not in self.standard:
-                self.standard.append(ftype["Standard"])
-        self.product_type = (self.product_type[0]
-                             if len(self.product_type) == 1
-                             else None
-                             )
-        self.standard = (self.standard[0] if len(self.standard) == 1 else None)
-
-        self.keyword_table = QTableView()
-
-# --------------------
-
-
-class FitsKeywordModel(QAbstractTableModel):
-    """
-    Create a QAbstractTableModel to display Pandas DataFrame objects in a
-    QTableView widget.
-    """
-
-    def __init__(self, keyword_list, parent=None):
-        """
-        Upon initialization, move the data into a numpy array (faster) and set
-        up variables.
-        """
 
         super().__init__(parent)
-        pdframe = keyword_list.to_dataframe()
-        self._data = np.array(pdframe.values)
-        self._ind = pdframe.index
-        self._cols = pdframe.columns
-        self._row_count, self._col_count = np.shape(self._data)
-        if self._ind.dtype == "int64":
-            self._ind = [str(x) for x in range(1, self._row_count+1)]
+        self.master = parent
 
-    def rowCount(self, parent=None):
-        return self._row_count
+        cwd = os.getcwd()
+        self.templates_dir = "/".join([cwd, TEMPLATES_DIR])
+        self.standard_template = None
 
-    def columnCount(self, parent=None):
-        return self._col_count
+        update_button = gb.GreenButton("Update from current .hlsp file", 70)
+        update_button.clicked.connect(self.load_current_fits)
 
-    def data(self, index, role=Qt.DisplayRole):
-        if index.isValid():
-            if role == Qt.DisplayRole:
-                return self._data[index.row(), index.column()]
-        return None
+        self.display_grid = QGridLayout()
 
-    def headerData(self, p_int, orientation, role):
-        if role == Qt.DisplayRole:
-            if orientation == Qt.Horizontal and self.columnCount() > 0:
-                return self._cols[p_int]
-            elif orientation == Qt.Vertical and self.rowCount() > 0:
-                return self._ind[p_int]
-        return None
+        standard_fits_label = QLabel("Standard FITS Keyword")
+        caom_keyword_label = QLabel("CAOM Keyword")
+        alternate_fits_label = QLabel("Alternate FITS Keyword")
+        header_num_label = QLabel("Header #")
+        default_val_label = QLabel("Default")
+
+        self._keywords = []
+        self._standard_fits_col = 0
+        self._caom_keyword_col = 1
+        self._alternate_fits_col = 2
+        self._header_num_col = 3
+        self._default_val_col = 4
+
+        label_row = 1
+        self._first_filetype = self._next_filetype = (label_row+1)
+
+        self.display_grid.addWidget(standard_fits_label,
+                                    label_row,
+                                    self._standard_fits_col
+                                    )
+        self.display_grid.addWidget(caom_keyword_label,
+                                    label_row,
+                                    self._caom_keyword_col
+                                    )
+        self.display_grid.addWidget(alternate_fits_label,
+                                    label_row,
+                                    self._alternate_fits_col
+                                    )
+        self.display_grid.addWidget(header_num_label,
+                                    label_row,
+                                    self._header_num_col
+                                    )
+        self.display_grid.addWidget(default_val_label,
+                                    label_row,
+                                    self._default_val_col
+                                    )
+
+        self.meta_grid = QGridLayout()
+        self.meta_grid.addWidget(update_button, 0, 0)
+        self.meta_grid.addLayout(self.display_grid, 1, 0)
+
+        self.setLayout(self.meta_grid)
+        self.show()
+
+    def _add_filetype_row(self, fits_key=None, info=None):
+
+        fits_kw = QLabel()
+        caom_kw = QLabel()
+        alt_fits_kw = QLineEdit()
+        hdr_num = QLineEdit()
+        def_val = QLineEdit()
+
+        if fits_key and info:
+            new_keyword_obj = FitsKeyword(fits_key, parameters=info)
+            self._keywords.append(new_keyword_obj)
+            fits_kw.setText(fits_key)
+            caom_kw.setText(info["caom_keyword"])
+            hdr_num.setText(str(info["header"]))
+            try:
+                def_val.setText(str(info["default"]))
+            except KeyError:
+                pass
+
+        self.display_grid.addWidget(fits_kw,
+                                    self._next_filetype,
+                                    self._standard_fits_col
+                                    )
+        self.display_grid.addWidget(caom_kw,
+                                    self._next_filetype,
+                                    self._caom_keyword_col
+                                    )
+        self.display_grid.addWidget(alt_fits_kw,
+                                    self._next_filetype,
+                                    self._alternate_fits_col
+                                    )
+        self.display_grid.addWidget(hdr_num,
+                                    self._next_filetype,
+                                    self._header_num_col
+                                    )
+        self.display_grid.addWidget(def_val,
+                                    self._next_filetype,
+                                    self._default_val_col
+                                    )
+
+        self.display_grid.setRowStretch(self._next_filetype, 0)
+        self._next_filetype += 1
+        self.display_grid.setRowStretch(self._next_filetype, 1)
+
+    def _get_current_standard(self):
+
+        standards = self.master.hlsp.member_fits_standards()
+        if standards is None:
+            std_label = QLabel("No FITS standard found!")
+        elif len(standards) > 1:
+            std_label = QLabel("More than 1 FITS standard found!")
+        else:
+            std_label = QLabel("Loaded {0} keywords".format(standards[0]))
+        self.display_grid.addWidget(std_label, 0, 0)
+
+        std = ".".join([standards[0], "yml"])
+        self.standard_template = "/".join([self.templates_dir, std])
+
+    def _load_standard_template(self):
+
+        if not self.standard_template:
+            return
+
+        template = read_yaml(self.standard_template)
+        keywords = template["KEYWORDS"]
+
+        for key, info in keywords.items():
+            self._add_filetype_row(key, info)
+
+    def _read_filetype_row(self, row_num):
+
+        std_fits = self.display_grid.itemAtPosition(row_num,
+                                                    self._standard_fits_col
+                                                    )
+        caom_kw = self.display_grid.itemAtPosition(row_num,
+                                                   self._caom_keyword_col
+                                                   )
+        new_fits = self.display_grid.itemAtPosition(row_num,
+                                                    self._alternate_fits_col
+                                                    )
+        hdr_num = self.display_grid.itemAtPosition(row_num,
+                                                   self._header_num_col
+                                                   )
+        default = self.display_grid.itemAtPosition(row_num,
+                                                   self._default_val_col
+                                                   )
+
+        row_info_dict = {}
+        alternates = new_fits.widget().text()
+        alternates = [a.strip() for a in alternates.split(",")]
+        if len(alternates) == 1 and alternates[0] == "":
+            alternates = None
+        row_info_dict["alternates"] = alternates
+        row_info_dict["header"] = hdr_num.widget().text()
+        row_info_dict["default"] = default.widget().text()
+        if row_info_dict["default"] == "":
+            row_info_dict["default"] = None
+
+        for kw_obj in self._keywords:
+            if kw_obj.fits_keyword == std_fits.widget().text():
+                update_flag = kw_obj.update(row_info_dict)
+                if update_flag:
+                    print("UpdateKeywordsGUI found update")
+                    self.master.hlsp.add_keyword_update(kw_obj)
+
+    def load_current_fits(self):
+        self._get_current_standard()
+        self._load_standard_template()
+
+    def update_hlsp_file(self):
+
+        for row in range(self._first_filetype, self._next_filetype):
+            self._read_filetype_row(row)
---
+++ gui/metadata.py
@@ -0,0 +1,235 @@
+import gui.GUIbuttons as gb
+import os
+import sys
+
+from bin.read_yaml import read_yaml
+from CHECK_METADATA_FORMAT.check_metadata_format import check_metadata_format
+from CHECK_METADATA_FORMAT.precheck_data_format import precheck_data_format
+from lib.FileType import FileType
+
+try:
+    from PyQt5.QtCore import *
+    from PyQt5.QtWidgets import *
+except ImportError:
+    from PyQt4.QtCore import *
+    from PyQt4.QtGui import *
+
+TEMPLATES_DIR = "CHECK_METADATA_FORMAT/TEMPLATES"
+
+
+class CheckMetadataGUI(QWidget):
+
+    def __init__(self, parent):
+
+        super().__init__(parent)
+        self.master = parent
+
+        self.selected_count = 0
+
+        cwd = os.getcwd()
+        templates_dir = "/".join([cwd, TEMPLATES_DIR])
+        templates = os.listdir(templates_dir)
+        self.standards = [t.split(".")[0] for t in templates]
+
+        precheck_button = gb.GreenButton("Pre-check File Metadata", 70)
+        precheck_button.clicked.connect(self.precheck_clicked)
+        self.precheck_grid = QGridLayout()
+        self.precheck_grid.addWidget(precheck_button, 0, 0)
+
+        file_ending_head = QLabel("File Ending:")
+        standard_head = QLabel("Standard:")
+        data_type_head = QLabel("Data Type:")
+        file_type_head = QLabel("File Type:")
+        product_type_head = QLabel("Product Type:")
+        check_meta_head = QLabel("Check Metadata?")
+        mrp_head = QLabel("MRP?")
+        self.files_grid = QGridLayout()
+        self.files_grid.addWidget(file_ending_head, 0, 0)
+        self.files_grid.addWidget(standard_head, 0, 1)
+        self.files_grid.addWidget(data_type_head, 0, 2)
+        self.files_grid.addWidget(file_type_head, 0, 3)
+        self.files_grid.addWidget(product_type_head, 0, 4)
+        self.files_grid.addWidget(check_meta_head, 0, 5)
+        self.files_grid.addWidget(mrp_head, 0, 6)
+        self._first_file = self._next_file = 1
+
+        self.metacheck_button = gb.GreenButton(
+            "Check Metadata of Selected File(s)", 70)
+        self.metacheck_button.clicked.connect(self.metacheck_clicked)
+        self.metacheck_button.setEnabled(False)
+        self.check_grid = QGridLayout()
+        self.check_grid.addWidget(self.metacheck_button, 0, 0)
+
+        self.type_count_label = QLabel()
+        self.meta_grid = QGridLayout()
+        self.meta_grid.addLayout(self.precheck_grid, 0, 0)
+        self.meta_grid.addWidget(self.type_count_label, 1, 0)
+        self.meta_grid.addLayout(self.files_grid, 2, 0)
+        self.meta_grid.addLayout(self.check_grid, 3, 0)
+
+        self.meta_grid.setRowStretch(0, 0)
+        self.meta_grid.setRowStretch(1, 0)
+        self.meta_grid.setRowStretch(2, 1)
+
+        self.setLayout(self.meta_grid)
+        self.show()
+
+    def _add_file_row(self, new_file):
+
+        ft = QLabel(new_file.ftype)
+
+        if self._next_file == self._first_file:
+            standard = QComboBox()
+            standard.addItems(sorted(self.standards))
+            standard.currentIndexChanged.connect(self._set_data_types)
+        else:
+            standard = QLabel()
+
+        data_type = QComboBox()
+        data_type.addItems(new_file.valid_pt)
+        data_type.setCurrentText(new_file.product_type)
+
+        file_type = QComboBox()
+        file_type.addItems(new_file.valid_ft)
+        file_type.setCurrentText(new_file.file_type)
+
+        product_type = QComboBox()
+        product_type.addItems(new_file.valid_cpt)
+        product_type.setCurrentText(new_file.caom_product_type)
+
+        toggle_meta = QCheckBox()
+        toggle_meta.setChecked(new_file.run_check)
+        if new_file.run_check:
+            self.selected_count += 1
+        toggle_meta.stateChanged.connect(self._update_selected)
+
+        toggle_mrp = QCheckBox()
+        toggle_mrp.setChecked(new_file.mrp_check)
+
+        self.files_grid.addWidget(ft, self._next_file, 0)
+        self.files_grid.addWidget(standard, self._next_file, 1)
+        self.files_grid.addWidget(data_type, self._next_file, 2)
+        self.files_grid.addWidget(file_type, self._next_file, 3)
+        self.files_grid.addWidget(product_type, self._next_file, 4)
+        self.files_grid.addWidget(toggle_meta, self._next_file, 5)
+        self.files_grid.addWidget(toggle_mrp, self._next_file, 6)
+
+        self._next_file += 1
+
+    def _del_file_row(self):
+
+        self._next_file -= 1
+        n_elements = self.files_grid.columnCount()
+        for n in range(n_elements):
+            x = self.files_grid.itemAtPosition(self._next_file, n).widget()
+            x.setParent(None)
+
+    def _read_file_row(self, row_num):
+
+        ending = self.files_grid.itemAtPosition(row_num, 0).widget()
+        std = self.files_grid.itemAtPosition(self._first_file, 1).widget()
+        dt = self.files_grid.itemAtPosition(row_num, 2).widget()
+        ft = self.files_grid.itemAtPosition(row_num, 3).widget()
+        pt = self.files_grid.itemAtPosition(row_num, 4).widget()
+        chk = self.files_grid.itemAtPosition(row_num, 5).widget().checkState()
+        mrp = self.files_grid.itemAtPosition(row_num, 6).widget().checkState()
+
+        for ft_obj in self.master.hlsp.file_types:
+            if ft_obj.ftype == ending.text():
+                ft_obj.standard = std.currentText().split("_")[-1]
+                ft_obj.product_type = dt.currentText()
+                ft_obj.file_type = ft.currentText()
+                ft_obj.caom_product_type = pt.currentText()
+                ft_obj.run_check = (True if chk == Qt.Checked else False)
+                ft_obj.mrp_check = (True if mrp == Qt.Checked else False)
+
+    def _set_data_types(self):
+
+        std = self.files_grid.itemAtPosition(self._first_file, 1).widget()
+        dt = std.currentText().split("_")[0]
+        for row in range(self._first_file, self._next_file):
+            data_type = self.files_grid.itemAtPosition(row, 2).widget()
+            data_type.setCurrentText(dt)
+
+    def _toggle_prechecked(self):
+
+        if self._next_file == self._first_file or self.selected_count == 0:
+            self.metacheck_button.setEnabled(False)
+            self.master.hlsp.ingest["metadata_prechecked"] = False
+        else:
+            self.metacheck_button.setEnabled(True)
+            self.master.hlsp.ingest["metadata_prechecked"] = True
+
+    def _update_selected(self, state):
+        """
+        sender = self.sender()
+        ind = self.files_grid.indexOf(sender)
+        pos = self.files_grid.getItemPosition(ind)
+        row = pos[0]
+        standard_col = 1
+        std = self.files_grid.itemAtPosition(row, standard_col).widget()
+        """
+
+        if state == Qt.Checked:
+            self.selected_count += 1
+            # std.setVisible(True)
+        else:
+            self.selected_count -= 1
+            # std.setVisible(False)
+
+        self._toggle_prechecked()
+
+    def clear_files(self):
+
+        while self._next_file > self._first_file:
+            self._del_file_row()
+        self.selected_count = 0
+
+    def display_files(self, types_list):
+
+        for ftype in types_list:
+            name = list(ftype.keys())
+            if len(name) > 1:
+                continue
+            else:
+                name = name[0]
+            # print("<<<metadata sending>>>{0}".format(ftype[name]))
+            as_obj = FileType(name, param_dict=ftype[name])
+            # print("<<<as_obj>>>{0}".format(as_obj.as_dict()))
+            self.master.hlsp.add_filetype(as_obj)
+            self._add_file_row(as_obj)
+
+        self._toggle_prechecked()
+
+    def metacheck_clicked(self):
+
+        self.update_hlsp_file()
+        check_metadata_format(self.master.hlsp.as_dict(), is_file=False)
+
+    def precheck_clicked(self):
+
+        hlsp_dir = self.master.hlsp.file_paths["InputDir"]
+        hlsp_name = self.master.hlsp.hlsp_name
+        results = precheck_data_format(hlsp_dir, hlsp_name)
+        results_dict = read_yaml(results)
+        types_found = results_dict["FileTypes"]
+        self.type_count_label.setText(
+            "...found {0} file types".format(len(types_found)))
+        # print("<<<metadata GUI found>>> {0}".format(types_found))
+        self.clear_files()
+        self.display_files(types_found)
+
+    def update_hlsp_file(self):
+
+        for n in range(self._first_file, self._next_file):
+            self._read_file_row(n)
+        self.master.hlsp.save()
+
+
+# --------------------
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = CheckMetadataGUI(parent=None)
+    sys.exit(app.exec_())
